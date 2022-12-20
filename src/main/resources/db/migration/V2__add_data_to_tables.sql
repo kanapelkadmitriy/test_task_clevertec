2f75785736a522dd99379aa82469de2c5e24feab
dkanapelka <dkonopelko748@gmail.com>
2022-12-20 19:08:54 +0300

    fix migration data to tables

--- src/main/resources/db/migration/V2__add_data_to_tables.sql
+++ src/main/resources/db/migration/V2__add_data_to_tables.sql
@@ -1,12 +1,12 @@
 INSERT INTO product(description, price, quantity, is_discounted)
 VALUES ('bacon', 100, 11, false),
-       ('pike', 200, 20, false);
+       ('pike', 200, 20, true);
        ('beans', 150, 34, false);
-       ('onion', 110, 20, false);
+       ('onion', 110, 20, true);
        ('lime', 200, 31, false);
-       ('rice', 100, 10, false);
+       ('rice', 100, 10, true);
        ('cheese', 120, 33, false);
-       ('cake', 140, 58, false);
+       ('cake', 140, 58, true);
 
 INSERT INTO card(number, discount)
 VALUES ('card-1234', 10),
