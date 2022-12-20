8a02cf5083ed6640a365513234191433972efeb3
dkanapelka <dkonopelko748@gmail.com>
2022-12-20 18:31:13 +0300

    add migration data to tables

---
+++ src/main/resources/db/migration/V2__add_data_to_tables.sql
@@ -0,0 +1,16 @@
+INSERT INTO product(description, price, quantity, is_discounted)
+VALUES ('bacon', 100, 11, false),
+       ('pike', 200, 20, false);
+       ('beans', 150, 34, false);
+       ('onion', 110, 20, false);
+       ('lime', 200, 31, false);
+       ('rice', 100, 10, false);
+       ('cheese', 120, 33, false);
+       ('cake', 140, 58, false);
+
+INSERT INTO card(number, discount)
+VALUES ('card-1234', 10),
+       ('card-5678', 15);
+       ('card-7634', 20);
+       ('card-8120', 25);
+       ('card-3791', 30);
