264d92818b1c227e34e6f63ad82a3490f0dd8fb2
dkanapelka <dkonopelko748@gmail.com>
2023-03-23 21:33:41 +0300

    add ReceiptWriting to pdf file

--- src/main/java/com/example/test_task_clevertec/service/impl/PdfReceiptWriterService.java
+++ src/main/java/com/example/test_task_clevertec/service/impl/PdfReceiptWriterService.java
@@ -3,6 +3,10 @@ package com.example.test_task_clevertec.service.impl;
 import com.example.test_task_clevertec.model.dto.ReceiptResponseDto;
 import com.example.test_task_clevertec.service.ReceiptWriterService;
 import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfImportedPage;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfWriter;
 import lombok.RequiredArgsConstructor;
@@ -12,8 +16,10 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
+import static com.example.test_task_clevertec.util.Constants.RECEIPT_PATH;
 import static com.example.test_task_clevertec.util.Constants.TEMPLATE_PDF_PATH;
 
 @Slf4j
@@ -22,16 +28,45 @@ import static com.example.test_task_clevertec.util.Constants.TEMPLATE_PDF_PATH;
 @RequiredArgsConstructor
 public class PdfReceiptWriterService implements ReceiptWriterService {
 
+    private static final String PDF = ".pdf";
+    private static final int LEADING = 15;
+    private static final int SIZE = 15;
+    private static final int QUANTITY_OF_EMPTY_LINES = 15;
+    private static final int PAGE_NUMBER_OF_TEMPLATE = 1;
+    private static final int X0 = 0;
+    private static final int Y0 = 0;
+
     private final ReceiptDataGenerator receiptDataGenerator;
 
     @SneakyThrows
     @Override
     public void write(ReceiptResponseDto receiptResponse) {
         final List<String> receiptData = receiptDataGenerator.generateReceiptData(receiptResponse);
-        PdfReader reader = new PdfReader(TEMPLATE_PDF_PATH);
-        System.out.println("----");
+        String fileName = receiptData.get(1).substring(2);
+
+        final Document document = new Document();
+        final PdfWriter pdfWriter = PdfWriter.getInstance(document,
+                new FileOutputStream(RECEIPT_PATH + fileName + PDF));
+        document.open();
+        importTemplate(pdfWriter, TEMPLATE_PDF_PATH);
+        writeReceiptData(receiptData, document);
+        document.close();
     }
 
+    private void importTemplate(PdfWriter pdfWriter, String templatePath) throws IOException {
+        final PdfReader reader = new PdfReader(templatePath);
+        final PdfImportedPage importedPage = pdfWriter.getImportedPage(reader, PAGE_NUMBER_OF_TEMPLATE);
+        pdfWriter.getDirectContent().addTemplate(importedPage, X0, Y0);
+    }
+
+
+    private void writeReceiptData(List<String> receiptData, Document document) throws DocumentException {
+        final Font font = new Font(Font.FontFamily.TIMES_ROMAN, SIZE, Font.NORMAL);
+        final Paragraph paragraph = new Paragraph();
+        paragraph.add(new Paragraph("\n".repeat(QUANTITY_OF_EMPTY_LINES)));
+        receiptData.forEach(line -> paragraph.add(new Paragraph(LEADING, line, font)));
+        document.add(paragraph);
+    }
 
 
 }
--- src/main/java/com/example/test_task_clevertec/service/impl/StandardReceiptDataGenerator.java
+++ src/main/java/com/example/test_task_clevertec/service/impl/StandardReceiptDataGenerator.java
@@ -12,52 +12,52 @@ import java.util.List;
 public class StandardReceiptDataGenerator implements ReceiptDataGenerator {
 
     private static final String DOLLAR_SYMBOL = "$";
-    private static final String RECEIPT_PATH = "receipt/";
     private static final String SEPARATE_SYMBOL = "=";
     private static final String SPACE_SYMBOL = " ";
-    private static final String QTY_TITLE = "QTY";
-    private static final String DESCRIPTION_TITLE = "DESCRIPTION";
-    private static final String PRICE_TITLE = "PRICE";
-    private static final String TOTAL_TITLE = "TOTAL";
+//    private static final String QTY_TITLE = "QTY";
+//    private static final String DESCRIPTION_TITLE = "DESCRIPTION";
+//    private static final String PRICE_TITLE = "PRICE";
+//    private static final String TOTAL_TITLE = "TOTAL";
+    private static final String QTY_TITLE = "qty";
+    private static final String DESCRIPTION_TITLE = "description";
+    private static final String PRICE_TITLE = "price";
+    private static final String TOTAL_TITLE = "total";
     private static final String SPACE = "    ";
 
     @Override
     public List<String> generateReceiptData(ReceiptResponseDto receiptResponse) {
         final List<String> receiptData = new ArrayList<>();
-        String title = QTY_TITLE +
-                getLineBySymbol(SPACE_SYMBOL, SPACE.length()) +
-                DESCRIPTION_TITLE +
-                getLineBySymbol(SPACE_SYMBOL, SPACE.length()) +
-                PRICE_TITLE +
-                getLineBySymbol(SPACE_SYMBOL, SPACE.length()) +
-                getLineBySymbol(SPACE_SYMBOL, SPACE.length()) +
-                TOTAL_TITLE;
-        int totalReceiptWidth = title.length();
+        final int qtyLength = getLengthOfQty(receiptResponse);
+        final int descriptionLength = getLengthOfDescription(receiptResponse);
+        final int priceLength = getLengthOfPrice(receiptResponse);
+        final int totalLength = getLengthOfTotal(receiptResponse);
 
+        int totalReceiptWidth = qtyLength + SPACE.length() +
+                descriptionLength + SPACE.length() +
+                priceLength + SPACE.length() +
+                totalLength;
         receiptData.add("CASH RECEIPT");
-        receiptData.add("№ " + receiptResponse.getReceiptNumber());
+        receiptData.add("N " + receiptResponse.getReceiptNumber());
         receiptData.add("Date: " + receiptResponse.getDate());
         receiptData.add("Time: " + receiptResponse.getTime());
         receiptData.add(getLineBySymbol(SEPARATE_SYMBOL, totalReceiptWidth));
-        receiptData.add(title);
-        for (ProductReceiptDto productReceiptDto : receiptResponse.getProducts()) {
-            String itemSb = productReceiptDto.getQuantity() +
-                    getLineBySymbol(SPACE_SYMBOL,
-                            (QTY_TITLE + SPACE).length() - String.valueOf(productReceiptDto.getQuantity()).length()) +
-                    productReceiptDto.getDescription() +
-                    getLineBySymbol(SPACE_SYMBOL,
-                            (DESCRIPTION_TITLE + SPACE).length() - productReceiptDto.getDescription().length()) +
-                    DOLLAR_SYMBOL +
-                    productReceiptDto.getPrice() +
-                    getLineBySymbol(SPACE_SYMBOL,
-                            (PRICE_TITLE + SPACE).length() - String.valueOf(productReceiptDto.getPrice()).length()) +
-                    DOLLAR_SYMBOL +
-                    productReceiptDto.getTotal();
 
+        String title = lineWithSpaceGenerate(QTY_TITLE, qtyLength) +
+                lineWithSpaceGenerate(DESCRIPTION_TITLE, descriptionLength) +
+                lineWithSpaceGenerate(PRICE_TITLE, priceLength) +
+                TOTAL_TITLE;
+        receiptData.add(title);
+
+        for (ProductReceiptDto productReceiptDto : receiptResponse.getProducts()) {
+            String itemSb = lineWithSpaceGenerate(String.valueOf(productReceiptDto.getQuantity()), qtyLength) +
+                    lineWithSpaceGenerate(productReceiptDto.getDescription(), descriptionLength) +
+                    lineWithSpaceGenerate(productReceiptDto.getPrice() + DOLLAR_SYMBOL, descriptionLength) +
+                    productReceiptDto.getTotal() + DOLLAR_SYMBOL;
             receiptData.add(itemSb);
         }
+
         receiptData.add(getLineBySymbol(SEPARATE_SYMBOL, totalReceiptWidth));
-        receiptData.add("TAXABLE TOT."
+                receiptData.add("TAXABLE TOT."
                 + getLineBySymbol(SPACE_SYMBOL,
                 totalReceiptWidth - "TAXABLE TOT.".length()
                         - (DOLLAR_SYMBOL + receiptResponse.getTotalSum()).length())
@@ -73,6 +73,46 @@ public class StandardReceiptDataGenerator implements ReceiptDataGenerator {
                 + DOLLAR_SYMBOL + receiptResponse.getTotal());
         return receiptData;
     }
+
+    private int getLengthOfQty(ReceiptResponseDto receiptResponseDto) {
+        final int lengthOfQty = receiptResponseDto.getProducts().stream()
+                .mapToInt(productReceiptDto -> String.valueOf(productReceiptDto.getQuantity()).length())
+                .max()
+                .orElse(QTY_TITLE.length());
+        return Math.max(lengthOfQty, QTY_TITLE.length());
+    }
+
+    private int getLengthOfDescription(ReceiptResponseDto receiptResponseDto) {
+        final int lengthOfQty = receiptResponseDto.getProducts().stream()
+                .mapToInt(productReceiptDto -> String.valueOf(productReceiptDto.getDescription()).length())
+                .max()
+                .orElse(DESCRIPTION_TITLE.length());
+        return Math.max(lengthOfQty, DESCRIPTION_TITLE.length());
+    }
+
+    private int getLengthOfPrice(ReceiptResponseDto receiptResponseDto) {
+        final int lengthOfQty = receiptResponseDto.getProducts().stream()
+                .mapToInt(productReceiptDto -> (productReceiptDto.getPrice() + DOLLAR_SYMBOL).length())
+                .max()
+                .orElse(PRICE_TITLE.length());
+        return Math.max(lengthOfQty, PRICE_TITLE.length());
+    }
+
+    private int getLengthOfTotal(ReceiptResponseDto receiptResponseDto) {
+        final int lengthOfQty = receiptResponseDto.getProducts().stream()
+                .mapToInt(productReceiptDto -> (productReceiptDto.getTotal() + DOLLAR_SYMBOL).length())
+                .max()
+                .orElse(TOTAL_TITLE.length());
+        return Math.max(lengthOfQty, TOTAL_TITLE.length());
+    }
+
+    private String lineWithSpaceGenerate(String line, int maxLength) {
+        int quantityOfAdditionSpaces = maxLength - line.length();
+        return quantityOfAdditionSpaces > 0 ?
+                line + getLineBySymbol(SPACE_SYMBOL, quantityOfAdditionSpaces) + SPACE
+                : line + SPACE;
+    }
+
 
     private String getLineBySymbol(String symbol, int symbolWidth) {
         return Strings.repeat(symbol, symbolWidth);
